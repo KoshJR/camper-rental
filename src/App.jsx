2ff7d90e0724b2c6e232b2edca3f7c095f28e112
KoshJR <135273624+KoshJR@users.noreply.github.com>
2024-05-05 13:12:44 -0400

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
-
-import Layout from './components/Layout/Layout'
-import { Routes, Route, Navigate } from "react-router-dom"
-import HomePage from './pages/HomePage'
-import CatalogPage from './pages/CatalogPage'
-import FavoritesPage from './pages/FavoritesPage'
+import Layout from "./components/Layout/Layout";
+import { Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CatalogPage from "./pages/CatalogPage";
+import FavoritesPage from "./pages/FavoritesPage";
 
 export const App = () => {
-
-
   return (
     <Layout>
       <Routes>
-        <Route path="=/" element={<HomePage />} />
-        <Route path="=/catalog" element={<CatalogPage />} />
-        <Route path="=/favorites" element={<FavoritesPage />} />
-        <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        {/* <Route path="*" element={<Navigate to="/" />} /> */}
       </Routes>
     </Layout>
   );
-}
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "../src/styles/index.css";
-import "../src/styles/fonts.css"
+import "../src/styles/fonts.css";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store.js";
 import { BrowserRouter } from "react-router-dom";
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor} loading={null}>
-        <BrowserRouter basename="/">
+        <BrowserRouter basename="camper-rental/">
           <App />
         </BrowserRouter>
       </PersistGate>
