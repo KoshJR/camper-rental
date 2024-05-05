b29dc204308df3e5362bb477529ce065c27cfb67
KoshJR <135273624+KoshJR@users.noreply.github.com>
2024-05-05 13:01:14 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import Layout from './components/Layout/Layout'
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import HomePage from './pages/HomePage'
 import CatalogPage from './pages/CatalogPage'
 import FavoritesPage from './pages/FavoritesPage'
@@ -11,12 +11,13 @@ export const App = () => {
   return (
     <Layout>
       <Routes>
-        <Route path='camper-rental/' element={<HomePage />} />
-        <Route path='camper-rental/catalog' element={<CatalogPage/> } />
-        <Route path='camper-rental/favorites' element={<FavoritesPage/> } />
-     </Routes>
+        <Route path="=/" element={<HomePage />} />
+        <Route path="=/catalog" element={<CatalogPage />} />
+        <Route path="=/favorites" element={<FavoritesPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
     </Layout>
-  )
+  );
 }
 
 export default App
