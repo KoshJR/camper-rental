235f4ff404ff5f0dc1b69d24a3d0590a73f5b827
KoshJR <135273624+KoshJR@users.noreply.github.com>
2024-05-05 14:05:06 -0400

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,13 +6,7 @@ const HomePage = () => {
     <div className={css.homeMessage}>
       <h1 className={css.mainTitle}>Wilderness Wheels</h1>
       <p className={css.welcomeText}>
-        <span>
-          {" "}
-          <a className={css.pLink} href="camper-rental/catalog">
-            Find your dream camp!
-          </a>
-        </span>{" "}
-        and start your Journey today!{" "}
+        <span> Find your dream camp,</span> and start your Journey today!{" "}
       </p>
     </div>
   );
